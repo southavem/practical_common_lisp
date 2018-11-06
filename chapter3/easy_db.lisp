ea94f35b6035eb9db64aed9135d3e75204548ff5
井上航 <inoue.wataru.1028@gmail.com>
2018-11-06 15:09:42 +0900

    commit

--- chapter3/easy_db.lisp
+++ chapter3/easy_db.lisp
@@ -8,3 +8,14 @@
 (defun dump-db ()
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (parse-integer (prompt-read "Rating"))
+   (prompt-read "Ripped [y/n]")))
